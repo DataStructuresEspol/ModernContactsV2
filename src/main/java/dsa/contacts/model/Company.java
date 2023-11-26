bdcffcea0ab427bb3fa7980e9b943557f4dbd999
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-11-25 19:16:04 -0500

    some changes

--- src/main/java/dsa/contacts/model/Company.java
+++
@@ -1,9 +0,0 @@
-package dsa.contacts.model;
-
-public class Company extends Contact{
-    private String name;
-
-    public Company(int id) {
-        super(id);
-    }
-}
--- src/main/java/dsa/contacts/model/Country.java
+++
@@ -1,7 +0,0 @@
-package dsa.contacts.model;
-
-public class Country {
-    private String name;
-    private String code;
-
-}
