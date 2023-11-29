247176e1e8182211f01cc6f204295008caf7465f
unknown <erillope@espol.edu.ec>
2023-11-29 00:09:08 -0500

    Visualizacion contactos

--- src/main/java/dsa/contacts/controllers/ContactInfoController.java
+++ src/main/java/dsa/contacts/controllers/ContactInfoController.java
@@ -1,4 +1,48 @@
 package dsa.contacts.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class ContactInfoController {
+
+    @FXML
+    private ImageView favoriteIcon;
+    @FXML
+    private Label nameLable;
+    @FXML
+    private ImageView profilePic;
+    @FXML
+    private Label phoneLabel;
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private Label addressLabel;
+    @FXML
+    private Label socialMediaLabel;
+
+    @FXML
+    private void retroceder(MouseEvent event) {
+    }
+
+    @FXML
+    private void setFavorite(MouseEvent event) {
+    }
+
+    @FXML
+    private void deleteContact(MouseEvent event) {
+    }
+
+    @FXML
+    private void editContact(MouseEvent event) {
+    }
+
+    @FXML
+    private void nextPic(MouseEvent event) {
+    }
+
+    @FXML
+    private void prevPic(MouseEvent event) {
+    }
 }
