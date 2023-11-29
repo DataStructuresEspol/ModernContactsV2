8c0c38afc9f81eed8870c7c75177364cd51664d2
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-11-29 01:09:07 -0500

    VCard service

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module dsa.contacts {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires com.googlecode.ezvcard;
 
     opens dsa.contacts to javafx.fxml;
     opens dsa.contacts.controllers to javafx.fxml;
